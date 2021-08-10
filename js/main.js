a5e875913b57ccf2c3406a8b9e5e6499e7fd980f
Mohsen-Hussen <mohsenhussen8@gmail.com>
2021-08-10 14:11:38 +0300

    uploading project

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$('.carousel').flipster({
+    style:'carousel',
+    spacing:-0.4,
+    loop: true,
+    autoplay: 3000
+});
+
+$('.docCard').flipster({
+    style:'carousel',
+    spacing:-0.4,
+    loop: true,
+    autoplay: 3000
+});
+
+$('.testCard').flipster({
+    style:'flat',
+    spacing:-0.4,
+    loop: true,
+    autoplay: 3000
+});
+
+$('.partner').flipster({
+    style:'flat',
+    spacing:-0.4,
+    loop: true,
+    autoplay: 3000
+});
+
+$('.clientTest').flipster({
+    style:'flat',
+    spacing:-0.4,
+    loop: true,
+    autoplay: 3000
+});
