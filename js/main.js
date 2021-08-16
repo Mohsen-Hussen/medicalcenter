4636806e032c348f9dd689e475b93da4ec643ffb
Mohsen-Hussen <mohsenhussen8@gmail.com>
2021-08-16 14:04:52 +0300

    add change theme color feature

--- js/main.js
+++ js/main.js
@@ -1,41 +1,59 @@
-$('.carousel').flipster({
-    style:'carousel',
-    spacing:-0.4,
-    loop: true,
-    autoplay: 3000
-});
+(function () {
+    //add class value to the page 
+    let localValue = localStorage.getItem('color-option');
+    console.log(localValue);
 
-$('.docCard').flipster({
-    style:'carousel',
-    spacing:-0.4,
-    loop: true,
-    autoplay: 3000
-});
+    if (localValue == null) {
+        $(':root').css('--logoBlue', '#0085CC');
+    } else {
+        $(':root').css('--logoBlue', localValue);
+    }
 
-$('.testCard').flipster({
-    style:'flat',
-    spacing:-0.4,
-    loop: true,
-    autoplay: 3000
-});
+    $('.carousel').flipster({
+        style:'carousel',
+        spacing:-0.4,
+        loop: true,
+        autoplay: 3000
+    });
 
-$('.partner').flipster({
-    style:'flat',
-    spacing:-0.4,
-    loop: true,
-    autoplay: 3000
-});
+    $('.docCard').flipster({
+        style:'carousel',
+        spacing:-0.4,
+        loop: true,
+        autoplay: 3000
+    });
 
-$('.clientTest').flipster({
-    style:'flat',
-    spacing:-0.4,
-    loop: true,
-    autoplay: 3000
-});
+    $('.testCard').flipster({
+        style:'flat',
+        spacing:-0.4,
+        loop: true,
+        autoplay: 3000
+    });
 
-// change website color
-const colorName = () => {
-    let color = document.querySelector('.switch .colors .list-inline-item');
-    let colorNameAttr = color.getAttribute("data-color");
-    console.log(colorNameAttr);
-}
+    $('.partner').flipster({
+        style:'flat',
+        spacing:-0.4,
+        loop: true,
+        autoplay: 3000
+    });
+
+    $('.clientTest').flipster({
+        style:'flat',
+        spacing:-0.4,
+        loop: true,
+        autoplay: 3000
+    });
+
+    //toogel palatte
+    $('.switch .icon').on('click', function () {
+        $('.switch .colors').toggleClass('d-none');
+    });
+    // change color
+    $('.switch .colors .list-inline-item').on('click', function () {
+        // console.log($(this).data('color'));
+        // let colorValue = $(this).data('color');
+        $(':root').css('--logoBlue', $(this).data('color'));
+        console.log($(this).data('color'));
+        localStorage.setItem('color-option', $(this).data('color'));
+    });
+}())
